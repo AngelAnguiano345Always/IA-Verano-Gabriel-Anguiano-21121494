8859ca8d2f882f49486279d7ac5b10c3dca32844
AngelAnguiano345Always <anguianomejiaangel@gmail.com>
2026-07-09 14:53:01 -0600

    Ejemplo en clase de opencv

---
+++ Apuntes y trabajos de clase/imagen.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+img = cv.imread('Sandrome.jpg', 0)
+print(img.shape)
+x,y = img.shape
+for i in range(x):
+    for j in range(y):
+        if (img[i,j]<155):
+            img[i,j]=0
+        else:
+            img[i,j]=255
+cv.imshow('img', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
